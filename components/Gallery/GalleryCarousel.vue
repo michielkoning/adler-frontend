44d31fd0c4b8aca565c8694964f4622938405f4f
Michiel Koning <mail@michielkoning.nl>
2020-03-09 06:42:49 +0100

    buttons

--- components/Gallery/GalleryCarousel.vue
+++ components/Gallery/GalleryCarousel.vue
@@ -1,10 +1,14 @@
 <template>
   <div :class="$style.wrapper">
-    <ul :class="$style.list">
+    <ul ref="list" :class="$style.list">
       <li v-for="item in gallery" :key="item.id" :class="$style.item">
         <img :src="item.heroLarge" alt="" />
       </li>
     </ul>
+    <div :class="$style['button-wrapper']">
+      <button :class="$style.btn" @click="scrollToPreviousPage">Vorige</button>
+      <button :class="$style.btn" @click="scrollToNextPage">Volgende</button>
+    </div>
   </div>
 </template>
 
@@ -14,6 +18,19 @@ export default {
     gallery: {
       type: Array,
       default: () => [],
+    },
+  },
+
+  methods: {
+    scrollToNextPage() {
+      const { list } = this.$refs
+      const size = list.clientWidth
+      list.scrollBy(size, 0)
+    },
+    scrollToPreviousPage() {
+      const { list } = this.$refs
+      const size = list.clientWidth
+      list.scrollBy(-size, 0)
     },
   },
 }
@@ -26,11 +43,21 @@ export default {
   scroll-snap-type: x mandatory;
   -webkit-overflow-scrolling: touch;
   overflow-x: scroll;
+  scroll-behavior: smooth;
 }
 
 .item {
   scroll-snap-align: start;
   flex: 0 0 100%;
   width: 100%;
+}
+
+.button-wrapper {
+  display: flex;
+  justify-content: space-between;
+}
+
+.btn {
+  font-size: 2em;
 }
 </style>
