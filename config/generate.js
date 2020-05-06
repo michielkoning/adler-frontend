543df0507c6c351812c741dd3a952e08a156b98d
Michiel Koning <michiel.koning@valtech.com>
2020-05-06 16:27:11 +0200

    form

--- config/generate.js
+++ config/generate.js
@@ -12,57 +12,57 @@ const getExcludesPages = () => {
 
 export default {
   fallback: true,
-  // async routes() {
-  //   const uri = `${apiUrl}graphql`
+  async routes() {
+    const uri = `${apiUrl}graphql`
 
-  //   const query = `
-  //     query Sitemap($notIn: [ID]) {
-  //       pages(first: 99, where: { notIn: $notIn }) {
-  //         edges {
-  //           node {
-  //             relativeUrl
-  //           }
-  //         }
-  //       }
-  //       posts(first: 99) {
-  //         edges {
-  //           node {
-  //             relativeUrl
-  //           }
-  //         }
-  //       }
-  //       rooms(first: 99) {
-  //         edges {
-  //           node {
-  //             relativeUrl
-  //           }
-  //         }
-  //       }
-  //       arrangements(first: 99) {
-  //         edges {
-  //           node {
-  //             relativeUrl
-  //           }
-  //         }
-  //       }
-  //     }
-  //   `
+    const query = `
+      query Sitemap($notIn: [ID]) {
+        pages(first: 99, where: { notIn: $notIn }) {
+          edges {
+            node {
+              relativeUrl
+            }
+          }
+        }
+        posts(first: 99) {
+          edges {
+            node {
+              relativeUrl
+            }
+          }
+        }
+        rooms(first: 99) {
+          edges {
+            node {
+              relativeUrl
+            }
+          }
+        }
+        arrangements(first: 99) {
+          edges {
+            node {
+              relativeUrl
+            }
+          }
+        }
+      }
+    `
 
-  //   const variables = {
-  //     notIn: getExcludesPages(),
-  //   }
+    const variables = {
+      notIn: getExcludesPages(),
+    }
 
-  //   const apolloFetch = createApolloFetch({ uri })
-  //   const result = await apolloFetch({ query, variables }) // all apolloFetch arguments are optional
-  //   const { pages, posts, rooms, arrangements } = result.data
-  //   const urls = [
-  //     ...pages.edges,
-  //     ...posts.edges,
-  //     ...rooms.edges,
-  //     ...arrangements.edges,
-  //   ]
-  //   return urls.map((url) => {
-  //     return url.node.relativeUrl
-  //   })
-  // },
+    const apolloFetch = createApolloFetch({ uri })
+    const result = await apolloFetch({ query, variables }) // all apolloFetch arguments are optional
+    const { pages, posts, rooms, arrangements } = result.data
+    const urls = [
+      ...pages.edges,
+      ...posts.edges,
+      ...rooms.edges,
+      ...arrangements.edges,
+    ]
+    return urls.map((url) => {
+      return url.node.relativeUrl
+    })
+  },
 }
