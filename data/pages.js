217500a43304d6bf42b3a23a12630627948be918
Michiel Koning <mail@michielkoning.nl>
2020-05-24 15:45:41 +0200

    pages

--- data/pages.js
+++ data/pages.js
@@ -1,47 +1,47 @@
 export const homePageId = {
   de: 22,
-  nl: 2948,
-  en: 2950,
+  nl: 3098,
+  en: 3100,
 }
 
 export const hotelPageId = {
   de: 10,
-  nl: 2892,
-  en: 2938,
+  nl: 3063,
+  en: 3102,
 }
 
 export const arrangementsPageId = {
   de: 8,
-  nl: 2891,
-  en: 2935,
+  nl: 3062,
+  en: 3106,
 }
 
 export const roomsPageId = {
   de: 1718,
-  nl: 2894,
-  en: 2952,
+  nl: 3065,
+  en: 3109,
 }
 
 export const blogPageId = {
   de: 26,
-  nl: 2907,
-  en: 2933,
+  nl: 3112,
+  en: 3113,
 }
 
 export const contactPageId = {
   de: 14,
-  nl: 2893,
-  en: 2927,
+  nl: 3064,
+  en: 3081,
 }
 
 export const environmentPageId = {
   de: 2003,
-  nl: 2994,
-  en: 2996,
+  nl: 3125,
+  en: 3084,
 }
 
 export const privacyPageId = {
-  de: 2900,
+  de: 3084,
   nl: 2930,
-  en: 2261,
+  en: 3127,
 }
