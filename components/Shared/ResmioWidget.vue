9588660aba8b009d61744d0b3e3b2d7edd25d238
Michiel Koning <mail@michielkoning.nl>
2025-06-08 19:56:23 +0200

    add lazy loading

--- components/Shared/ResmioWidget.vue
+++ components/Shared/ResmioWidget.vue
@@ -5,18 +5,17 @@ const { resmioWidget } = useAppConfig();
 <template>
   <sidebar-item-section id="related-arrangements" :title="$t('ourRestaurant')">
     <iframe
-      ref="frame"
       :src="resmioWidget"
-      :class="$style.resmio"
+      class="resmio"
       width="400"
       height="300"
-      :lazy="true"
+      loading="lazy"
       :title="$t('reserveForOurRestaurant')"
     />
   </sidebar-item-section>
 </template>
 
-<style lang="postcss" module>
+<style lang="postcss" scoped>
 .resmio {
   min-height: 24em;
   height: calc(100% - 3em);
