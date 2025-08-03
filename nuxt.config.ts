75e826ef7f0a04746017288fe9e506326ba90a0f
Michiel Koning <mail@michielkoning.nl>
2025-08-03 04:44:20 +0200

    add pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -83,7 +83,7 @@ export default defineNuxtConfig({
     "nuxt-schema-org",
     "@nuxt/fonts",
     "@nuxt/icon",
-    // "@vite-pwa/nuxt",
+    "@vite-pwa/nuxt",
   ],
   icon: {
     componentName: "NuxtIcon",
