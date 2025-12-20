c785a50d4d4f0a2884a742583f9772398f043770
Michiel Koning <mail@michielkoning.nl>
2025-12-20 21:09:25 +0100

    add post

--- server/api/form.post.ts
+++ server/api/form.post.ts
@@ -2,6 +2,7 @@ export default defineEventHandler(async (event) => {
   const body = await readBody(event)
 
   return await $fetch(body.page, {
+    method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
     },
