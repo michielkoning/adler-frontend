afa94b6c502566623c5d3829df8e1a1b7cf3ab64
Michiel Koning <github@michielkoning.nl>
2026-04-22 22:54:43 +0200

    Update siteDetails.js

--- data/siteDetails.js
+++ data/siteDetails.js
@@ -5,7 +5,7 @@ export const themeColor = '#98012e'
 export const resmioUrl =
   'https://app.resmio.com/familienhotel-gasthof-adler/widget?source=www.adler-lingenau.com&amp;backgroundColor=%23fff&amp;color=%23424242&amp;linkBackgroundColor=%2398012e'
 export const easyBookingUrl =
-  'https://www.easy-booking.at/bookingengine2/?embedded&backgroundColor=%2398012e&fontColor=%23000000/#/7731/2/'
+  'https://www.easy-booking.at/bookingengine2/?embedded&backgroundColor=%2398012e&fontColor=%23000000/#7731/2/'
 export const categories = ['travel']
 export const facebook = 'https://www.facebook.com/adler.hotel'
 export const twitter = '@Adler_Hotel'
