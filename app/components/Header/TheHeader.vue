69835413ef52b84ec6655b9b31994ffe3ee6cfd7
Michiel Koning <mail@michielkoning.nl>
2026-06-15 13:47:40 +0200

    scrolling header

--- app/components/Header/TheHeader.vue
+++ app/components/Header/TheHeader.vue
@@ -95,11 +95,28 @@ onUnmounted(() => {
 
 <style lang="css" scoped>
 header {
+  z-index: 99;
+  padding-block: var(--spacing-2);
   margin-bottom: var(--menu-height);
   color: var(--color-text);
-  background: var(--color-white);
+  background: var(--color-primary);
+
+  @container (not scroll-state(scrolled: none)) {
+    position: sticky;
+    top: 0;
+    transition: translate var(--transition);
+  }
+
+  @container scroll-state(scrolled: bottom) {
+    translate: 0 -100%;
+  }
+
+  @container scroll-state(scrolled: top) {
+    translate: 0 0;
+  }
 
   @media (--navigation-md) {
+    position: relative;
     margin: 0;
     background-color: transparent;
   }
