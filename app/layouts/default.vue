70457318558248cec8ca24cf50c64c97e8013770
Michiel Koning <github@michielkoning.nl>
2026-07-21 10:55:47 +0200

    webmanifest

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -86,7 +86,7 @@ onUnmounted(() => {
     ref="page"
     class="page"
   >
-    <!-- <nuxt-pwa-assets /> -->
+    <nuxt-pwa-manifest />
     <nuxt-route-announcer />
     <nuxt-loading-indicator color="var(--color-primary)" />
     <skip-links />
